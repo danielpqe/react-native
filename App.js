2969d18f2b3f1d7c1f823e44dde60ecd9051be45
danielpqe <danielpqe@gmail.com>
2018-12-19 16:02:32 -0500

    flat list

--- App.js
+++ App.js
@@ -1,53 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Text} from 'react-native';
+import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
+import SuggestionList from './src/videos/containers/suggestion-list';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Image
-            source={require('./assets/matrix.png')}
-            style={{width: 200, height: 80}}
-        />
-        <Text style={styles.welcome}>MATRIX INVENTARIOS APP!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <Home>
+          <Header>
+            <Text>Hola que hace</Text>
+          </Header>
+          <Text>Buscador</Text>
+          <Text>Categorías</Text>
+          <SuggestionList />
+        </Home>
+
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
