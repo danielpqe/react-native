242a2c6a68f9dbf9a7cf63b2cd8fe1c452a46dee
danielpqe <danielpqe@gmail.com>
2018-12-21 12:42:17 -0500

    starting with redux °O°

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {Text,View} from 'react-native';
+import {Text} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/category-list';
-import Video from 'react-native-video';
+import Player from './src/player/containers/player';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -22,32 +22,12 @@ export default class App extends Component<Props> {
             categoryList: categories
         })
     }
-
     render() {
     return (
         <Home>
           <Header>
-              <View
-                style={{
-                    flex: 1,
-                    height: 100,
-                }}
-              >
-              <Video
-                source={{
-                    uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
-                }}
-                style={{
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    top: 0
-                }}
-                resizeMode="contain"
-              />
-              </View>
-          <Text>Hola que hace</Text>
+          <Player/>
+          <Text>Hola</Text>
           </Header>
           <Text>Buscador</Text>
           <Text>Categorías</Text>
